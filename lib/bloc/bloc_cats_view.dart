1ca535f3924e2c5f9a02dc7da79e6675260f0235
Emir Demirli <emirdemirli@gmail.com>
2023-02-02 00:05:46 +0300

    first commit

---
+++ lib/bloc/bloc_cats_view.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class BlocCatsView extends StatefulWidget {
+  const BlocCatsView({super.key});
+
+  @override
+  State<BlocCatsView> createState() => _BlocCatsViewState();
+}
+
+class _BlocCatsViewState extends State<BlocCatsView> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
